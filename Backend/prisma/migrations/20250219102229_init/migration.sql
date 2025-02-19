03dd9009af4149fcfcd52a6299a18f3e7e5ba65a
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-02-19 16:17:37 +0530

    Backend Configuration

---
+++ Backend/prisma/migrations/20250219102229_init/migration.sql
@@ -0,0 +1,91 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'Employee', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "EmployeeStatus" AS ENUM ('Active', 'Inactive');
+
+-- CreateEnum
+CREATE TYPE "AttendanceStatus" AS ENUM ('Present', 'Absent', 'HalfDay');
+
+-- CreateEnum
+CREATE TYPE "LeaveType" AS ENUM ('SickLeave', 'PaidLeave', 'CasualLeave');
+
+-- CreateEnum
+CREATE TYPE "LeaveStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "dateOfJoining" TIMESTAMP(3) NOT NULL,
+    "contectNo" TEXT NOT NULL,
+    "address" TEXT,
+    "status" "EmployeeStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attendance" (
+    "id" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "checkIn" TIMESTAMP(3),
+    "checkOut" TIMESTAMP(3),
+    "status" "AttendanceStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attendance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Leave" (
+    "id" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "leaveType" "LeaveType" NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "reason" TEXT,
+    "status" "LeaveStatus" NOT NULL DEFAULT 'PENDING',
+    "approvedBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Leave_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_userId_key" ON "Employee"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Attendance_employeeId_date_key" ON "Attendance"("employeeId", "date");
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attendance" ADD CONSTRAINT "Attendance_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Leave" ADD CONSTRAINT "Leave_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
